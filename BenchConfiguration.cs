2934123f20aaa4ee95c305f05c76fa071a1bee87
qnc082 <navedahmad082@gmail.com>
2026-05-25 00:15:39 +0530

    chore: modernize C# standards - file-scoped namespaces, editorconfig, analysis level, AGENTS.md, benchmark results

--- BenchConfiguration.cs
+++ BenchConfiguration.cs
@@ -1,14 +1,42 @@
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Validators;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Linq;
 
-namespace CSharpBenchLab
+namespace CSharpBenchLab;
+
+public class BenchConfiguration : ManualConfig
 {
-    public class BenchConfiguration : ManualConfig
+    public BenchConfiguration()
     {
-        public BenchConfiguration()
-        {
-            AddExporter(HtmlExporter.Default);
-            ArtifactsPath = Path.Combine(AppContext.BaseDirectory, "Docs/Results");
-        }
+        // Add standard loggers, column providers, etc. from DefaultConfig, but skip exporters
+        AddLogger(DefaultConfig.Instance.GetLoggers().ToArray());
+        AddAnalyser(DefaultConfig.Instance.GetAnalysers().ToArray());
+        AddJob(DefaultConfig.Instance.GetJobs().ToArray());
+        AddValidator(DefaultConfig.Instance.GetValidators().ToArray());
+
+        // Explicitly add columns and column providers to ensure they show up
+        AddColumnProvider(DefaultColumnProviders.Instance);
+        AddColumn(StatisticColumn.Mean, StatisticColumn.StdDev, StatisticColumn.Error);
+        
+        // Add MemoryDiagnoser to the config to ensure memory columns are present
+        AddDiagnoser(MemoryDiagnoser.Default);
+            
+        // EXPORTERS: We explicitly bypass DefaultConfig Exporters and ONLY add Markdown
+        AddExporter(MarkdownExporter.GitHub);
+
+        //Uncomment to Disable .log file creation (if you don't want the raw log text)
+        // Options |= ConfigOptions.DisableLogFile;
+        ArtifactsPath = Path.Combine(GetProjectRoot(), "Docs/Results");
+    }
+
+    private string GetProjectRoot([CallerFilePath] string filePath = "")
+    {
+        return Path.GetDirectoryName(filePath) ?? AppContext.BaseDirectory;
     }
 }
--- Data/MockDataGenerator.cs
+++ Data/MockDataGenerator.cs
@@ -1,26 +1,25 @@
 using Bogus;
 using CSharpBenchLab.Data.Models;
 
-namespace CSharpBenchLab.Data
+namespace CSharpBenchLab.Data;
+
+public static class MockDataGenerator
 {
-    public static class MockDataGenerator
+    private static readonly string[] Departments = new[]
     {
-        private static readonly string[] Departments = new[]
-        {
-            "Engineering", "Sales", "Marketing", "HR", "Finance", "Support", "IT"
-        };
+        "Engineering", "Sales", "Marketing", "HR", "Finance", "Support", "IT"
+    };
 
-        public static List<Employee> GenerateEmployees(int count)
-        {
-            var id = 1;
-            var faker = new Faker<Employee>()
-                .RuleFor(e => e.Id, f => id++)
-                .RuleFor(e => e.Name, f => f.Name.FullName())
-                .RuleFor(e => e.Department, f => f.PickRandom(Departments))
-                .RuleFor(e => e.Salary, f => Math.Round(f.Random.Decimal(40000, 150000), 2))
-                .RuleFor(e => e.DateHired, f => f.Date.Past(15, DateTime.Today.AddDays(-30)));
+    public static List<Employee> GenerateEmployees(int count)
+    {
+        var id = 1;
+        var faker = new Faker<Employee>()
+            .RuleFor(e => e.Id, f => id++)
+            .RuleFor(e => e.Name, f => f.Name.FullName())
+            .RuleFor(e => e.Department, f => f.PickRandom(Departments))
+            .RuleFor(e => e.Salary, f => Math.Round(f.Random.Decimal(40000, 150000), 2))
+            .RuleFor(e => e.DateHired, f => f.Date.Past(15, DateTime.Today.AddDays(-30)));
 
-            return faker.Generate(count);
-        }
+        return faker.Generate(count);
     }
 }
